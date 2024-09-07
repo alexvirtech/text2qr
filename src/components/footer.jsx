667bc4df53dd2206114576b96618887be3f88179
alexvirtech <alex@vir-tec.net>
2024-09-07 13:22:45 +0300

    Sat 09-07-24

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,6 +8,8 @@ export default function Footer() {
         <div class="h-[50px] p-3 bg-slate-200 border-t border-slate-300 text-sm flex items-center justify-center gap-1">            
             <div>All rights reserved to</div>            
             <div class="font-bold">&copy;AK</div>
+            {/* <div>GitHub:</div>
+            <a href="https://github.com/alexvirtech/text2qr/tree/main" target="_blank">https://github.com/alexvirtech/text2qr/tree/main</a> */}
         </div>
     )
 }
